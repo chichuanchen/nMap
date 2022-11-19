90c0f34afe3116041df3042775d4c0d7817f035a
Chi-Chuan <46586524+chichuanchen@users.noreply.github.com>
2022-11-19 12:48:27 -0600

    Initial scripts

---
+++ acc.R
@@ -0,0 +1,39 @@
+# SETUP
+library("lme4")
+library("car")
+
+
+rm(list = ls())
+load("./tidied/nmap_data_tidied.RData") 
+
+# wide format acc already calculated in a tibble, now output
+write.csv(nmap_acc.wide, file = "acc_subj_6cond_wide.csv", row.names = FALSE)
+#
+
+acc.by.subj.cond <- nmap_data %>%
+  group_by(Subject, cond) %>%
+  summarise(individual_acc = mean(correct_or_not, na.rm=T))
+
+write.csv(acc.by.subj.cond, file = "acc_subj_cond.csv")
+
+
+acc.by.subj.cond %>%
+  ggplot(aes(x=cond, y=individual_acc)) +
+  geom_jitter()
+
+acc.by.subj.cond %>%
+  group_by(cond) %>%
+  summarise(group_acc = mean(individual_acc)) %>%
+  ungroup() %>%
+  mutate(cond = factor(cond, levels = c("close", "med", "far"))) %>%
+  ggplot(aes(x=cond, y=group_acc, group = 1)) +
+  geom_point() +
+  geom_line() +
+  scale_y_continuous(breaks=seq(0.0, 1, 0.1), limits=c(0.3,0.7))
+  
+
+
+mym <- glmer(correct_or_not ~ cond + (1|Subject), family = "binomial", data = nmap_data)
+summary(mym)
+Anova(mym)
+
---
+++ create_working_tibble.R
@@ -0,0 +1,78 @@
+# Set up
+library("tidyverse")
+library("readxl")
+
+rm(list=ls())
+setwd(dir="../data/beh")
+
+# filter data using this list of subjects
+data.subjlist <- read_csv("BEHAVIORAL_ERPGOOD_SUBLIST.csv", col_names = T) %>%
+  select(SUBNUMB, PREPOST)
+
+table(data.subjlist$PREPOST)
+
+# variables of interest
+my_vars <- c("Subject", "Session", "Trial", "image[SubTrial]", "sound1[SubTrial]", "tag", 
+             "animalpic.RESP", "respdisp.RESP")
+
+# prep pre and post data sets
+data.pre <- read_csv("nmap_merged_PRE.csv", col_names = T) %>%
+  select(my_vars) %>%
+  rename(stim.image = `image[SubTrial]`,
+         stim.sound = `sound1[SubTrial]`) %>%
+  filter(Session == 1, # make sure all observations are from session 1
+         Subject %in% data.subjlist$SUBNUMB[data.subjlist$PREPOST==1])  %>% # filter subjects whose ERP data are usable
+  drop_na(stim.image)
+
+data.post <- read_csv("nmap_merged_POST.csv", col_names = T) %>%
+  select(my_vars) %>%
+  rename(stim.image = `image[SubTrial]`,
+         stim.sound = `sound1[SubTrial]`) %>%
+  filter(Session==2, # make sure all observations are from session 2
+         Subject %in% data.subjlist$SUBNUMB[data.subjlist$PREPOST==2]) %>% # filter subjects whose ERP data are usable
+  drop_na(stim.image)
+
+# combine datasets and tidy
+# long format
+data.long <- bind_rows(data.pre, data.post) %>%
+  separate(tag, into = c("cue", "probe"), sep = "0", convert = T) %>%
+  mutate(
+    inconsist.resp = 
+      case_when(
+        animalpic.RESP == respdisp.RESP ~ 0,
+        is.na(animalpic.RESP) | is.na(respdisp.RESP) ~ 0,
+        TRUE ~ 1),
+    cresp = 
+      case_when(
+        cue < probe ~ 4,
+        cue > probe ~ 1),
+    correct_or_not = 
+      case_when(
+        animalpic.RESP == cresp | respdisp.RESP == cresp ~ 1, # if response during either pic or after pic is correct, then is correct
+        TRUE ~ 0), # the rest is incorrect (including inconsistent responses)
+    cond = 
+      case_when(
+        cue == 1 & probe == 2 ~ "med",
+        cue == 2 & probe == 1 ~ "med",
+        cue == 2 & probe == 3 ~ "close",
+        cue == 3 & probe == 2 ~ "close",
+        cue == 1 & probe == 3 ~ "far",
+        cue == 3 & probe == 1 ~ "far"),
+    order = if_else(cue < probe, "asc", "desc")
+        ) %>%
+  select(Subject, Session, Trial, cue, probe, order, cond, cresp, inconsist.resp, correct_or_not, stim.image, stim.sound)
+
+# wide format
+data.wide <- data.long %>%
+  unite("condition", c(cue, probe), sep = "_") %>%
+  group_by(Subject, Session, condition) %>%
+  summarise(individual_acc = mean(correct_or_not)) %>%
+  pivot_wider(names_from = condition,
+              values_from = individual_acc)
+
+# Save and export ----
+## rename
+nmap_data.long <- data.long
+nmap_acc.wide <- data.wide
+
+save(nmap_data.long, nmap_acc.wide, file="./tidied/nmap_data_tidied.RData")
