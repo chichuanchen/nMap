e25ce8acd442106424a7c7feb98bada92ab0a47b
Chi-Chuan <46586524+chichuanchen@users.noreply.github.com>
2023-02-13 14:26:38 -0600

    Add files via upload
    Creating working data frame for cognitive ability measures (conflict processing, working memory, inhibition, vocab)

---
+++ beh_create_working_tibble.R
@@ -0,0 +1,31 @@
+#######################
+# This script reads in behavioral data (language, EFs) given by Dan
+# and outputs data frames with individual performance in both long and wide format
+#######################
+
+# Set up
+library("tidyverse")
+library("readxl")
+
+rm(list=ls())
+setwd(dir="../")
+
+############################
+
+
+#########################
+# variables of interest
+my_vars <- c("PREPOST", "SUB", 
+             "WM", "CONFLICT", "INHIBIT", "VOCABRAW")
+
+data.raw <- read_excel("newseg_medAD_11.16.22.xlsx") %>%
+  select(all_of(my_vars)) %>%
+  rename(subj_num = SUB,
+         time_point = PREPOST)
+
+
+# Save and export ----
+## rename
+beh_data.raw <- data.raw
+
+save(beh_data.raw, file="./beh/tidied/beh_data_tidied.RData")
