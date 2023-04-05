f4c0eec189eba7aa24c59679dcdadffc219abdc9
Chi-Chuan <46586524+chichuanchen@users.noreply.github.com>
2023-04-05 12:56:27 -0500

    Update script with correct new data
    Previous data did not include time point 2 EF measures (values were all from time point 1). Data provided by Dan.

--- beh_create_working_tibble.R
+++ beh_create_working_tibble.R
@@ -8,24 +8,41 @@ library("tidyverse")
 library("readxl")
 
 rm(list=ls())
-setwd(dir="../")
+setwd(dir="../data/beh/")
 
 ############################
 
 
 #########################
 # variables of interest
-my_vars <- c("PREPOST", "SUB", "SEX", "AGE", "KL",
-             "WM", "CONFLICT", "INHIBIT", "VOCABRAW")
+my_vars <- c("PREPOST", "SUB", "SEX", "AGEPRE", "KL",
+             "WMPRE", "CONFLICTPRE", "INHIBITPRE", "VOCABRAWPRE",
+             "WMPOST", "CONFLICTPOST", "INHIBITPOST")
 
-data.raw <- read_excel("newseg_medAD_11.16.22.xlsx") %>%
+data.raw <- read_excel("newseg_medAD_4.5.23.xlsx") %>%
   select(all_of(my_vars)) %>%
   rename(subj_num = SUB,
          time_point = PREPOST)
 
+# tidy - 
+data.tidy <- data.raw %>%
+  mutate(WMPOST = as.numeric(WMPOST), # automatically turn non-numeric into NA
+         CONFLICTPOST = as.numeric(CONFLICTPOST),
+         INHIBITPOST = as.numeric(INHIBITPOST)) %>%
+  mutate(
+    WM = case_when(time_point == 1 ~ WMPRE,
+                   time_point == 2 ~ WMPOST),
+    CONFLICT = case_when(time_point == 1 ~ CONFLICTPRE,
+                         time_point == 2 ~ CONFLICTPOST),
+    INHIBIT = case_when(time_point == 1 ~ INHIBITPRE,
+                         time_point == 2 ~ INHIBITPOST),
+    VOCAB = case_when(time_point == 1 ~ VOCABRAWPRE, F ~ NA)) %>%
+  select(!ends_with(c("PRE", "POST")))
+
+glimpse(data.tidy)
 
 # Save and export ----
 ## rename
-beh_data.raw <- data.raw
+beh_data.raw <- data.tidy
 
-save(beh_data.raw, file="./beh/tidied/beh_data_tidied.RData")
+save(beh_data.raw, file="./beh_data_tidied.RData")
