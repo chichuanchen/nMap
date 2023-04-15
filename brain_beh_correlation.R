46c74d859d88976d2bdb46995c79083cf3ac4774
Chi-Chuan <46586524+chichuanchen@users.noreply.github.com>
2023-04-15 09:20:21 -0500

    Update brain_beh_correlation.R

--- brain_beh_correlation.R
+++ brain_beh_correlation.R
@@ -9,35 +9,24 @@ library("emmeans") # extract estimated marginal means
 # library("MuMIn") # for calculating r squared from LMEs
 
 rm(list = ls())
-load("../../tidied/erp_tidied.RData")
-load("../../tidied/DAN_erp_tidied.RData")
-load("../../../beh/beh_data_tidied.RData")
+load("./data/ERP/tidied/erp_tidied.RData")
+load("./data/beh/beh_data_tidied.RData")
 
+#
+data_erp_all <- data_erp_all %>%
+  mutate(KL.cat = 
+           case_when(
+             KL.cont %in% c(1:4) ~ "SS",
+             KL.cont %in% c(5:8) ~ "CP",
+             TRUE ~ as.character(NA)))
 
 # calculate individual (subj_num and time_point) mean ERP amplitude for different components
 # N1 sensitivity to visual quantity 3 - 1 (= far - close)
 indi_n1_sensitivity2 <- data_erp_all %>%
   filter(component == "n1") %>%
-  mutate(KL.cat = if_else(KL.cont %in% c(1:4), "SS", 
-                          if_else(KL.cont %in% c(5:8), "CP", NA))) %>%
-  # group_by(subj_num, time_point, cond) %>%
-  # mutate(cond.mean.amp = mean(amp)) %>%
-  # distinct(subj_num, time_point, KL.cat, cardinal, cond.mean.amp) %>%
   group_by(subj_num, time_point, KL.cat, cardinal) %>%
-  summarise(cardinal.mean.amp = mean(amp, na.rm = T)) %>%
-
+  summarise(cardinal.mean.amp = mean(amp)) %>%
   summarise(n1.mean.sensitivity.3_1 = (cardinal.mean.amp[cardinal==3]) - (cardinal.mean.amp[cardinal==1]))
-
-# N2 sensitivity to exact numerical distance (distance 1 minus distance 2)
-indi_n2_sensitivity <- data_erp_all %>%
-  filter(component == "n2") %>%
-  mutate(KL.cat = if_else(KL.cont < 5, "SS", "CP")) %>%
-  group_by(subj_num, time_point, KL.cat, distance) %>%
-  summarise(distance.mean.amp = mean(amp, na.rm = T)) %>%
-  group_by(subj_num, time_point, KL.cat) %>%
-  summarise(mean.sensitivity = 
-              (distance.mean.amp[distance=="1"]) - 
-              (distance.mean.amp[distance=="2"]))
 
 # combine erp and beh info
 bbcor_n1_beh <- full_join(indi_n1_sensitivity2, beh_data.raw) %>%
@@ -53,61 +42,25 @@ bbcor_n1_beh <- full_join(indi_n1_sensitivity2, beh_data.raw) %>%
 corr.n1sen.inhib <- bbcor_n1_beh %>%
   drop_na(n1.mean.sensitivity.3_1, INHIBIT)
 
-write.csv(corr.n1sen.inhib, "./../corr.n1sen.inhib_0413.csv")
-
-corr.n1sen.WM <- bbcor_n1_beh %>%
+corr.n1sen.wm <- bbcor_n1_beh %>%
   drop_na(n1.mean.sensitivity.3_1, WM)
 
+corr.n1sen.conflict <- bbcor_n1_beh %>%
+  drop_na(n1.mean.sensitivity.3_1, CONFLICT)
+# write.csv(corr.n1sen.inhib, "./../corr.n1sen.inhib_0413.csv")
+
+
 cor.test(corr.n1sen.inhib$n1.mean.sensitivity.3_1, corr.n1sen.inhib$INHIBIT, method = "pearson")
-cor.test(corr.n1sen.WM$n1.mean.sensitivity.3_1, corr.n1sen.WM$WM, method = "pearson")
-
-
-glimpse(bbcor_n1_beh)
-
-bbcor_n1_beh.CP <- bbcor_n1_beh %>% filter(KL.cat == "CP")
-bbcor_n1_beh.SS <- bbcor_n1_beh %>% filter(KL.cat == "SS")
+cor.test(corr.n1sen.wm$n1.mean.sensitivity.3_1, corr.n1sen.wm$WM, method = "pearson")
+cor.test(corr.n1sen.conflict$n1.mean.sensitivity.3_1, corr.n1sen.conflict$CONFLICT, method = "pearson")
 
 
 ggplot(data = bbcor_n1_beh, aes(x=INHIBIT, y=n1.mean.sensitivity.3_1)) +
   geom_point() +
-  geom_smooth(method="lm")
+  geom_smooth(method="lm",se = F) +
+  labs(x="Inhibition score", y="N1 sensitivity to visual number array") +
+  xlim(0.5, 1)
   
-
-ggplot(data = bbcor_n1_beh.SS, aes(x=INHIBIT, y=n1.mean.sensitivity.3_1)) +
-  geom_point() +
-  geom_smooth(method="lm")
-
-mymodel <- lmer(n1.mean.sensitivity.3_1 ~ INHIBIT.c + WM.c + CONFLICT.c + (1|time_point), 
-                data=bbcor_n1_beh) # singular fit
-
-mymodel <- lm(n1.mean.sensitivity.3_1 ~ INHIBIT.c, data=bbcor_n1_beh) 
-
-mymodel <- lm(n1.mean.sensitivity.3_1 ~ INHIBIT.c + time_point , 
-                data=bbcor_n1_beh)
-
-summary(mymodel)
-
-confint(mymodel, oldNames = FALSE)
-
-mymodel.SS <- lm(mean.sensitivity ~ INHIBIT.c + WM.c + CONFLICT.c, 
-              data=subset(bbcor_n1_beh, KL.cat == "SS"))
-summary(mymodel.SS)
-
-mymodel.CP <- lm(mean.sensitivity ~ INHIBIT.c + WM.c + CONFLICT.c, 
-                 data=subset(bbcor_n1_beh, KL.cat == "CP"))
-summary(mymodel.CP)
-
-coef(mymodel)["(Intercept)"]
-# fixef(mymodel)["(Intercept)"]
-
-ggplot(data=bbcor_n1_beh, aes(x=INHIBIT.c, y=mean.sensitivity,color=KL.cat)) + 
-  geom_point() +
-  geom_smooth(method = "lm") +
-  # geom_abline(intercept = coef(mymodel)["(Intercept)"], slope = coef(mymodel)["INHIBIT.c"]) +
-  labs(x="Inhibition Score", y="N1 Sensitivity to Visual Quantities") +
-  facet_grid(~time_point)
-
-
 
 # 
 # combine erp and beh info
@@ -135,6 +88,19 @@ ggplot(data=bbcor_n2_beh, aes(x=INHIBIT.c, y=mean.sensitivity)) +
   geom_point() +
   geom_abline(intercept = coes[1], slope = coes[2]) +
   labs(x="Inhibition Score", y="N1 Sensitivity to Visual Quantities")
+
+
+
+# N2 sensitivity to exact numerical distance (distance 1 minus distance 2)
+indi_n2_sensitivity <- data_erp_all %>%
+  filter(component == "n2") %>%
+  mutate(KL.cat = if_else(KL.cont < 5, "SS", "CP")) %>%
+  group_by(subj_num, time_point, KL.cat, distance) %>%
+  summarise(distance.mean.amp = mean(amp, na.rm = T)) %>%
+  group_by(subj_num, time_point, KL.cat) %>%
+  summarise(mean.sensitivity = 
+              (distance.mean.amp[distance=="1"]) - 
+              (distance.mean.amp[distance=="2"]))
 
 
 # N2 sensitivity to EF load (visual quantity 2 (cond 2 and 3) - visual quantity of 1 and 3 (cond 1 4 5 6))
