6aca1d61469acf4300d98479584248c3715e1b91
Chi-Chuan <46586524+chichuanchen@users.noreply.github.com>
2023-04-17 23:23:28 -0500

    Update ERPbeh_acc.R

--- ERPbeh_acc.R
+++ ERPbeh_acc.R
@@ -5,35 +5,57 @@ library("car")
 
 rm(list = ls())
 load("../data/ERPbeh/tidied/nmap_data_tidied.RData") 
+load("../data/beh/beh_data_tidied.RData")
 
 # wide format acc already calculated in a tibble, now output
 write.csv(nmap_acc.wide, file = "acc_subj_6cond_wide.csv", row.names = FALSE)
 #
 
 acc.by.subj.cond <- nmap_data.long%>%
-  group_by(Subject, cond) %>%
+  group_by(Subject, Session, cue, probe) %>%
   summarise(individual_acc = mean(correct_or_not, na.rm=T))
 
-write.csv(acc.by.subj.cond, file = "acc_subj_cond.csv")
+write.csv(acc.by.subj.cond, file = "acc_subj_6cond.csv")
 
 
-acc.by.subj.cond %>%
-  ggplot(aes(x=cond, y=individual_acc)) +
-  geom_jitter()
+acc.by.subj <- nmap_data.long%>%
+  group_by(Subject, Session) %>%
+  summarise(individual_acc = mean(correct_or_not, na.rm=T))
 
-acc.by.subj.cond %>%
-  group_by(cond) %>%
-  summarise(group_acc = mean(individual_acc)) %>%
-  ungroup() %>%
-  mutate(cond = factor(cond, levels = c("close", "med", "far"))) %>%
-  ggplot(aes(x=cond, y=group_acc, group = 1)) +
-  geom_point() +
-  geom_line() +
-  scale_y_continuous(breaks=seq(0.0, 1, 0.1), limits=c(0.3,0.7))
+write.csv(acc.by.subj, file = "acc_subj_overall.csv")
+
+
+# combine ERP acc with subj info
+
+ERPbeh_info <- acc.by.subj %>%
+  rename(subj_num = Subject,
+         time_point = Session) %>%
+  right_join(beh_data.raw, by = c("subj_num", "time_point")) %>%
+  mutate(KL.cat = 
+           case_when(
+             KL %in% c(1:4) ~ "SS",
+             KL %in% c(5:8) ~ "CP",
+             TRUE ~ as.character(NA)))
   
 
+ERPbeh_info %>%
+  group_by(KL.cat) %>%
+  summarise(mean.acc.KL = mean(individual_acc, na.rm=T),
+            sd.acc.KL = sd(individual_acc, na.rm=T))
 
-mym <- glmer(correct_or_not ~ cond + (1|Subject), family = "binomial", data = nmap_data.long)
-summary(mym)
-Anova(mym)
+ERPbeh_info.SS <- ERPbeh_info %>%
+  filter(KL.cat == "SS") %>%
+  ungroup() 
 
+t.test(ERPbeh_info.SS$individual_acc, mu=.5)
+
+ERPbeh_info.CP <- ERPbeh_info %>%
+  filter(KL.cat == "CP") %>%
+  ungroup() 
+
+t.test(ERPbeh_info.CP$individual_acc, mu=.5)
+
+
+ERPbeh_info %>%
+  t.test(individual_acc ~ KL.cat, data = .)
+  
