9a116bb3117987a0ccd4e65c5241056e4bb11b02
Chi-Chuan <46586524+chichuanchen@users.noreply.github.com>
2023-04-20 11:57:59 -0500

    Update ERPbeh_acc.R

--- ERPbeh_acc.R
+++ ERPbeh_acc.R
@@ -13,74 +13,84 @@ load("../data/ERP/tidied/erp_tidied.RData")
 # write.csv(nmap_acc.wide, file = "acc_subj_6cond_wide.csv", row.names = FALSE)
 
 # ERPbeh #######################################################################
-## Accuracy by condition (6) -----
+## Accuracy by condition (6) BASE-----
 
-acc.by.subj.cond <- nmap_data.long%>%
-  group_by(Subject, Session, cue, probe) %>%
-  summarise(individual_acc = mean(correct_or_not, na.rm=T)) %>%
-  ungroup()
+acc.by.subj.cond <- nmap_data.long %>%
+  rename(subj_num = Subject,
+         time_point = Session) %>%
+  group_by(subj_num, time_point, cue, probe) %>%
+  mutate(cond_acc = mean(correct_or_not, na.rm=T)) %>%
+  distinct(subj_num, time_point, cue, probe, cond_acc) %>%
+  ungroup() %>%
+  mutate(approx.dist = case_when(
+           cue > probe ~ cue/probe,
+           cue < probe ~ probe/cue),
+         exact.dist = abs(cue - probe))
 
 write.csv(acc.by.subj.cond, file = "../data/ERPbeh/ERPbeh_acc_subj_6cond.csv")
 
 ## Accuracy by subject (across all 6 conditions) -----
 
 acc.by.subj <-  acc.by.subj.cond %>%
-  group_by(Subject, Session) %>%
-  summarise(subj_acc = mean(individual_acc, na.rm=T)) %>%
+  group_by(subj_num, time_point) %>%
+  summarise(subj_acc = mean(cond_acc, na.rm=T)) %>%
   ungroup()
 
 write.csv(acc.by.subj, file = "../data/ERPbeh/ERPbeh_acc_subj.csv")
 
-## Accuracy by ratio effect (close minus far) -----
-acc.ratio.effect <- nmap_data.long %>%
-  rename(subj_num = Subject,
-         time_point = Session) %>%
-  group_by(subj_num, time_point, cue, probe) %>%
-  mutate(cond_acc = mean(correct_or_not, na.rm=T),
-         cond = factor(cond)) %>%
-  distinct(subj_num, time_point, cue, probe, cond, cond_acc) %>%
-  group_by(subj_num, time_point, cond) %>%
-  summarise(avg.acc.ratio = mean(cond_acc)) %>%
+## Accuracy by approximate distance effect (close minus far) -----
+acc.approx.dist.effect <- acc.by.subj.cond %>%
+  group_by(subj_num, time_point, approx.dist) %>%
+  summarise(avg.acc.approx.dist = mean(cond_acc)) %>%
   group_by(subj_num, time_point) %>%
-  summarise(ratio.acc = avg.acc.ratio[cond == "close"] - avg.acc.ratio[cond == "far"]) %>%
+  summarise(approx.dist.acc = avg.acc.approx.dist[approx.dist == 1.5] - avg.acc.approx.dist[approx.dist == 3]) %>%
   ungroup()
 
-write.csv(acc.ratio.effect, file = "../data/ERPbeh/ERPbeh_acc_ratioeffect.csv")
+write.csv(acc.approx.dist.effect, file = "../data/ERPbeh/ERPbeh_acc_approx_dist_effect.csv")
+
+## Accuracy by exact distance effect (2 - 1) -----
+acc.exact.dist.effect <- acc.by.subj.cond %>%
+  group_by(subj_num, time_point, exact.dist) %>%
+  summarise(avg.acc.exact.dist = mean(cond_acc)) %>%
+  group_by(subj_num, time_point) %>%
+  summarise(exact.dist.acc = avg.acc.exact.dist[exact.dist == "2"] - avg.acc.exact.dist[exact.dist == "1"]) %>%
+  ungroup()
+
+write.csv(acc.exact.dist.effect, file = "../data/ERPbeh/ERPbeh_acc_exact_dist_effect.csv")
 
 # ERPbeh x Beh #################################################################
 # combine ERPbeh acc with subj info (sex, KL, EF, age, vocab)
 
 ## By condition (6) -----
 ERPbeh_info_by.cond <- acc.by.subj.cond %>%
-  rename(subj_num = Subject,
-         time_point = Session) %>%
   right_join(beh_data.raw, by = c("subj_num", "time_point")) %>%
   mutate(KL.cat = 
            factor(case_when(
              KL %in% c(1:4) ~ "SS",
              KL %in% c(5:8) ~ "CP",
-             TRUE ~ as.character(NA))),
-         approx.dist = case_when(
-           cue > probe ~ cue/probe,
-           cue < probe ~ probe/cue
-         ),
-         exact.dist = abs(cue - probe))
+             TRUE ~ as.character(NA))))
 
 
 ## By exact distance (2) -----
-ERPbeh_info_by.exact.dist <- ERPbeh_info_by.cond %>%
-  group_by(subj_num, time_point, KL.cat, exact.dist) %>%
-  summarise(avg.acc.exact.dist = mean(individual_acc))
+ERPbeh_info_by.exact.dist <- acc.exact.dist.effect %>%
+  right_join(beh_data.raw, by = c("subj_num", "time_point")) %>%
+  mutate(KL.cat = 
+           factor(case_when(
+             KL %in% c(1:4) ~ "SS",
+             KL %in% c(5:8) ~ "CP",
+             TRUE ~ as.character(NA))))
 
 ## By approximate distance (3) -----
-ERPbeh_info_by.approx.dist <- ERPbeh_info_by.cond %>%
-  group_by(subj_num, time_point, KL.cat, approx.dist) %>%
-  summarise(avg.acc.approx.dist = mean(individual_acc))
+ERPbeh_info_by.approx.dist <- acc.approx.dist.effect %>%
+  right_join(beh_data.raw, by = c("subj_num", "time_point")) %>%
+  mutate(KL.cat = 
+           factor(case_when(
+             KL %in% c(1:4) ~ "SS",
+             KL %in% c(5:8) ~ "CP",
+             TRUE ~ as.character(NA))))
 
 ## By subject -----
 ERPbeh_info_by.subj <- acc.by.subj %>%
-  rename(subj_num = Subject,
-         time_point = Session) %>%
   right_join(beh_data.raw, by = c("subj_num", "time_point")) %>%
   mutate(KL.cat = 
            factor(case_when(
@@ -188,22 +198,43 @@ pvals <- c(pvals, cor.test(corr.acc_CONFLICT$subj_acc, corr.acc_CONFLICT$CONFLIC
 # Perform FDR correction
 p.adjust(pvals, method = "fdr")
 
-##### beh ratio effect and EF -----
+##### beh distance effect and EF -----
+###### approximate distance -----
 corr.ratio_WM <- ERPbeh_info_by.subj %>%
-  left_join(acc.ratio.effect, by = c("subj_num", "time_point")) %>%
-  drop_na(ratio.acc, WM) 
+  left_join(acc.approx.dist.effect, by = c("subj_num", "time_point")) %>%
+  drop_na(approx.dist.acc, WM) 
 
 corr.ratio_CONFLICT <- ERPbeh_info_by.subj %>%
-  left_join(acc.ratio.effect, by = c("subj_num", "time_point")) %>%
-  drop_na(ratio.acc, CONFLICT) 
+  left_join(acc.approx.dist.effect, by = c("subj_num", "time_point")) %>%
+  drop_na(approx.dist.acc, CONFLICT) 
 
 corr.ratio_INHIBIT <- ERPbeh_info_by.subj %>%
-  left_join(acc.ratio.effect, by = c("subj_num", "time_point")) %>%
-  drop_na(ratio.acc, INHIBIT) 
+  left_join(acc.approx.dist.effect, by = c("subj_num", "time_point")) %>%
+  drop_na(approx.dist.acc, INHIBIT) 
 
-pvals <- cor.test(corr.ratio_WM$ratio.acc, corr.ratio_WM$WM)$p.value
-pvals <- c(pvals, cor.test(corr.ratio_CONFLICT$ratio.acc, corr.ratio_CONFLICT$CONFLICT)$p.value)
-pvals <- c(pvals, cor.test(corr.ratio_INHIBIT$ratio.acc, corr.ratio_INHIBIT$INHIBIT)$p.value)
+pvals <- cor.test(corr.ratio_WM$approx.dist.acc, corr.ratio_WM$WM)$p.value
+pvals <- c(pvals, cor.test(corr.ratio_CONFLICT$approx.dist.acc, corr.ratio_CONFLICT$CONFLICT)$p.value)
+pvals <- c(pvals, cor.test(corr.ratio_INHIBIT$approx.dist.acc, corr.ratio_INHIBIT$INHIBIT)$p.value)
+
+# Perform FDR correction
+p.adjust(pvals, method = "fdr")
+
+###### exact distance -----
+corr.ratio_WM <- ERPbeh_info_by.subj %>%
+  left_join(acc.exact.dist.effect, by = c("subj_num", "time_point")) %>%
+  drop_na(exact.dist.acc, WM) 
+
+corr.ratio_CONFLICT <- ERPbeh_info_by.subj %>%
+  left_join(acc.exact.dist.effect, by = c("subj_num", "time_point")) %>%
+  drop_na(exact.dist.acc, CONFLICT) 
+
+corr.ratio_INHIBIT <- ERPbeh_info_by.subj %>%
+  left_join(acc.exact.dist.effect, by = c("subj_num", "time_point")) %>%
+  drop_na(exact.dist.acc, INHIBIT) 
+
+pvals <- cor.test(corr.ratio_WM$exact.dist.acc, corr.ratio_WM$WM)$p.value
+pvals <- c(pvals, cor.test(corr.ratio_CONFLICT$exact.dist.acc, corr.ratio_CONFLICT$CONFLICT)$p.value)
+pvals <- c(pvals, cor.test(corr.ratio_INHIBIT$exact.dist.acc, corr.ratio_INHIBIT$INHIBIT)$p.value)
 
 # Perform FDR correction
 p.adjust(pvals, method = "fdr")
