a93a56158d5adb2c891fc065c879752ce4d02ba0
Chi-Chuan <46586524+chichuanchen@users.noreply.github.com>
2023-06-27 18:21:29 +0200

    included 0-knowers as SS-knowers

--- brain_beh_correlation.R
+++ brain_beh_correlation.R
@@ -18,7 +18,7 @@ load("../data/ERPbeh/acc.exact.dist.effect.RData")
 data_erp_all <- data_erp_all %>%
   mutate(KL.cat = 
            case_when(
-             KL.cont %in% c(1:4) ~ "SS",
+             KL.cont %in% c(0:4) ~ "SS",
              KL.cont %in% c(5:8) ~ "CP",
              TRUE ~ as.character(NA)))
 
