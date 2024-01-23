f5ef1fb0058e138d246f0ebb61e448d48f2dffc3
Chi-Chuan <46586524+chichuanchen@users.noreply.github.com>
2024-01-22 20:03:21 -0600

    updated CP status change

--- beh_descriptive.R
+++ beh_descriptive.R
@@ -15,12 +15,47 @@ load("./../data/beh/tidied/beh_data_tidied.RData")
 # define categorical KL
 beh_data <- beh_data.raw %>%
   drop_na(KL) %>%
-  mutate(KL.cat = if_else(KL < 5, "SS", "CP")) 
+  mutate(KL.cat = 
+           case_when(
+             KL %in% c(0:4) ~ "SS",
+             KL %in% c(5:8) ~ "CP",
+             TRUE ~ as.character(NA)))
 
-#
+# some descriptive beh stats
+range(beh_data$WM, na.rm = T)
+range(beh_data$CONFLICT, na.rm = T)
+range(beh_data$INHIBIT, na.rm = T)
+range(beh_data$VOCAB, na.rm = T)
+range(beh_data$VOCAB.SS, na.rm = T)
+
+
+# drop out rate (only time point 1)?
+
 length(unique(beh_data$subj_num))
 table(duplicated(beh_data$subj_num))
-table(beh_data$KL.cat)
+table(beh_data$KL.cat) # 53/128 have 2 time points. 128-53=75 dropped out
+
+twotimers <- beh_data %>%
+  group_by(subj_num) %>%
+  filter(n() > 1)
+
+onetimers <- beh_data %>%
+  group_by(subj_num) %>%
+  filter(n() == 1)
+
+onlyPRE <- onetimers %>%
+  filter(time_point == 1)
+
+onlyPOST <- onetimers %>%
+  filter(time_point == 2)
+
+
+CPchange <- twotimers %>% 
+  filter(length(unique(KL.cat))==2) %>% 
+  select(subj_num, time_point, KL.cat) %>%
+  pivot_wider(names_from = time_point, values_from = KL.cat)
+
+
 t.test(beh_data$AGE~beh_data$KL.cat)
 
 beh_data %>%
